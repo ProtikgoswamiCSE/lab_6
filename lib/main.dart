2a6918ef11d43896c9ae4c3674ad335d9b10eb8c
ProtikgoswamiCSE <goswami15-5841@diu.edu.bd>
2025-02-26 11:25:03 +0600

    new

--- lib/main.dart
+++ lib/main.dart
@@ -31,20 +31,28 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       drawer: Drawer(
         child: ListView(
-          children: const [
-            DrawerHeader(
+          children: [
+            const DrawerHeader(
               decoration: BoxDecoration(color: Colors.deepPurple),
-              child: Text("Menu",
-                  style: TextStyle(color: Colors.white, fontSize: 24)),
+              child: Text(
+                "Menu",
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
             ),
             ListTile(
-              leading: Icon(Icons.lock_clock_outlined),
-              title: Text("Home"),
+              leading: const Icon(Icons.lock_clock),
+              title: const Text("Recent"),
+              onTap: () {
+                //Navigator.pop(context);
+              },
             ),
             ListTile(
-              leading: Icon(Icons.settings),
-              title: Text("Recent"),
-              onTap: () {},
+              leading: const Icon(
+                  Icons.image), // Changed to better represent "Recent"
+              title: const Text("Image"),
+              onTap: () {
+                //Navigator.pop(context);
+              },
             ),
           ],
         ),
@@ -55,16 +63,24 @@ class MyHomePage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          CachedNetworkImage(
-            imageUrl:
-                'https://images.unsplash.com/photo-1451187580459-43490279c0fa?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fDRrJTIwd2FsbHBhcGVyfGVufDB8fDB8fHww',
-            placeholder: (context, url) => const CircularProgressIndicator(),
-            errorWidget: (context, url, error) => const Icon(Icons.error),
+          Expanded(
+            child: CachedNetworkImage(
+              imageUrl:
+                  'https://images.unsplash.com/photo-1451187580459-43490279c0fa?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fDRrJTIwd2FsbHBhcGVyfGVufDB8fDB8fHww',
+              placeholder: (context, url) =>
+                  const Center(child: CircularProgressIndicator()),
+              errorWidget: (context, url, error) =>
+                  const Center(child: Icon(Icons.error)),
+            ),
           ),
-          CachedNetworkImage(
-            imageUrl: "http://via.placeholder.com/350x150",
-            placeholder: (context, url) => const CircularProgressIndicator(),
-            errorWidget: (context, url, error) => const Icon(Icons.error),
+          Expanded(
+            child: CachedNetworkImage(
+              imageUrl: "http://via.placeholder.com/350x150",
+              placeholder: (context, url) =>
+                  const Center(child: CircularProgressIndicator()),
+              errorWidget: (context, url, error) =>
+                  const Center(child: Icon(Icons.error)),
+            ),
           ),
         ],
       ),
