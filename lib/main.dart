f9b0ded2eb0362c7e93617df22f26ae7973fb27a
ProtikgoswamiCSE <goswami15-5841@diu.edu.bd>
2025-02-26 08:50:10 +0600

    new

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'HI'),
+      home: const MyHomePage(title: 'H'),
     );
   }
 }
